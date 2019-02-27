43dd0001c57d5f976ccaf47cc0d604dac8c68916
Wodann <remco.kuijper@gmail.com>
2019-02-27 15:28:47 -0500

    Linux compiler fix for FTDI

--- external/ftdi/include/WinTypes.h
+++ external/ftdi/include/WinTypes.h
@@ -43,7 +43,7 @@ typedef struct _OVERLAPPED {
         struct{
             DWORD Offset;
             DWORD OffsetHigh;
-        };
+        } CombinedOffset;
         PVOID  Pointer;
     };
     HANDLE hEvent;
