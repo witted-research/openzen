33ce37ab76d2553c9f5ef20540a80b1f6300ffd0
Tobias Schlüter <tobias@lp-research.com>
2019-03-20 14:42:11 +0900

    Fix DLL loading trouble.

--- src/utility/windows/WindowsDll.cpp
+++ src/utility/windows/WindowsDll.cpp
@@ -6,7 +6,10 @@
 #include <string>
 #include <type_traits>
 
+#include <gsl/gsl_util>
 #include <Windows.h>
+
+#include "FindThisModule.h"
 
 namespace fs = std::filesystem;
 
@@ -38,13 +41,38 @@ namespace zen
 
     void* WindowsDll::load(std::string_view filename)
     {
-        const auto filePath = fs::current_path().append(filename);
+        // Load the DLL which should be in the same directory as openzen.dll.
+        // The safest way to actually loading the DLL and all its dependencies
+        // appears to be using SetDllDirectory which gives us a deterministic
+        // search order.  AddDllDirectory -- if I take the documentation at
+        // face value, even though it is wrong in other cases that I inspected
+        // -- cannot be used because then dependent Dlls might not find their
+        // dependencies.
+        //
+        // So we remember the current DllDirectory, change it, try to load the
+        // DLL and set the DllDirectory back to its original value before we
+        // return.
+        // FIXME this is not threadsafe.
+
+        // Path to the openzen.dll.
+        fs::path path(zen::internal::FindThisModule());
+        path.remove_filename();
+
+        wchar_t c; // Null pointer argument to GetDllDirectory isn't
+                   // documented, so let's pass this as destination for
+                   // the length check.
+        DWORD bufLen = ::GetDllDirectoryW(1, &c);
+        std::vector<wchar_t> dllDirectoryBackup(bufLen);
+        ::GetDllDirectoryW(bufLen, dllDirectoryBackup.data());
+        ::SetDllDirectoryW(path.c_str());
+        auto resetDllDirectory = gsl::finally([&path, &dllDirectoryBackup]() {
+            SetDllDirectoryW(dllDirectoryBackup.data()); });
 
         // If already loaded
-        if (auto handle = ::GetModuleHandleW(filePath.c_str()))
+        if (auto handle = ::GetModuleHandleA(std::string(filename).c_str()))
             return handle;
 
-        if (auto handle = ::LoadLibraryW(filePath.c_str()))
+        if (auto handle = ::LoadLibraryA(std::string(filename).c_str()))
             return handle;
 
         return nullptr;
