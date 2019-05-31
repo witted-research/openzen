99df4013b1e2c8ac2e878c9398e1e59886de24f7
Thomas Hauth <thomas@lp-research.com>
2019-05-31 16:28:10 +0900

    added find this module code

---
+++ src/utility/windows/FindThisModule.h
@@ -0,0 +1,34 @@
+#ifndef FINDTHISMODULE_H_a48839fa_5146_4d3b_afda_d185e792951c
+#define FINDTHISMODULE_H_a48839fa_5146_4d3b_afda_d185e792951c
+
+#include <string>
+
+namespace zen {
+    namespace internal {
+        // Get the filename of the current DLL / Executable / Module.
+        std::wstring FindThisModule();
+
+        //
+        // Implementation details below.
+        //
+
+        // This needs an object that is guaranteed to be in the same file as the 
+        // caller, so has to live in the same executable where it's called.  This is
+        // guaranteed by using a variable in an anonymous namespace.
+        namespace {
+            char objectInThisModule;
+        }
+
+        // The actual worker.
+        std::wstring FindModuleForObject(const void *object);
+
+        // This calls the worker, passing it an address that is guaranteed
+        // to be living in the same module as the caller.
+        inline std::wstring FindThisModule()
+        {
+            return FindModuleForObject(&objectInThisModule);
+        }
+    }
+}
+
+#endif
