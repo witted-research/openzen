691ea9f719fcba6f832730be6322a8f4b7b3fc20
Thomas Hauth <thomas@lp-research.com>
2019-08-02 22:37:47 +0900

    setting log level to debug in unit test

--- src/test/OpenZenTests.cpp
+++ src/test/OpenZenTests.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 
+#include <spdlog/spdlog.h>
+
 int main(int argc, char **argv) {
+    spdlog::set_level(spdlog::level::level_enum::debug);
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
