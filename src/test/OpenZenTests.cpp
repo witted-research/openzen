c155eba9e85af412a4214f3a885556524b24556b
Thomas Hauth <thomas@lp-research.com>
2019-12-03 18:32:59 +0900

    enabling spdlog output in unit tests

--- src/test/OpenZenTests.cpp
+++ src/test/OpenZenTests.cpp
@@ -1,10 +1,15 @@
 #include <gtest/gtest.h>
 
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_sinks.h>
 
 int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    const std::string loggerName = "OpenZenTest_console";
+    auto console_logger = spdlog::stdout_logger_mt(loggerName);
+    spdlog::set_default_logger(console_logger);
     spdlog::set_level(spdlog::level::level_enum::debug);
 
-    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
