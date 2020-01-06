fdf91313f655d8e2707a605967b46cbf8aaac287
Tobias Schlüter <tobias@lp-research.com>
2020-01-06 17:29:57 +0900

    Fix uninitialized member.

--- src/io/interfaces/linux/LinuxDeviceInterface.cpp
+++ src/io/interfaces/linux/LinuxDeviceInterface.cpp
@@ -70,6 +70,7 @@ namespace zen
         , m_fd(fd)
         , m_terminate(false)
         , m_pollingThread(&LinuxDeviceInterface::run, this)
+        , m_baudRate(0)
     {
         std::memset(&m_readCB, 0, sizeof(aiocb64));
         m_readCB.aio_fildes = m_fd;
