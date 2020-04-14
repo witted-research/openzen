8158a04e70f3a2606a99f94ead0bca0a88f5b833
Thomas Hauth <thomas@lp-research.com>
2020-04-14 14:31:21 +0900

    added script to automatically add licence header

---
+++ add_license_header.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in $(find . -name "*.h" -or -name "*.cpp")
+do
+  if ! grep -q Copyright $i
+  then
+    echo "Will add copyright notice to $i"
+    cat license_header.txt $i >$i.new && mv $i.new $i
+  fi
+done
+
