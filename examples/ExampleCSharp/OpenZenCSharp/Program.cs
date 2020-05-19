c6eb3a5caed3d4f5a54c5ae0b06c1e303d394b98
Thomas Hauth <thomas@lp-research.com>
2020-05-19 14:56:05 +0900

    improved C# example

--- examples/ExampleCSharp/OpenZenCSharp/Program.cs
+++ examples/ExampleCSharp/OpenZenCSharp/Program.cs
@@ -51,12 +51,15 @@ namespace OpenZenCSharpBindingTest
                                         continue;
                                     // read acceleration
                                     OpenZenFloatArray fa = OpenZenFloatArray.frompointer(zenEvent.data.imuData.a);
+                                    // read angular velocity
+                                    OpenZenFloatArray fg = OpenZenFloatArray.frompointer(zenEvent.data.imuData.g);
                                     // read euler angles
                                     OpenZenFloatArray fr = OpenZenFloatArray.frompointer(zenEvent.data.imuData.r);
                                     // read quaternion
                                     OpenZenFloatArray fq = OpenZenFloatArray.frompointer(zenEvent.data.imuData.q);
 
-                                    Console.WriteLine("Sensor data\n -> Acceleration a = " + fa.getitem(0) + " " + +fa.getitem(1) + " " + fa.getitem(2));
+                                    Console.WriteLine("Sensor data\n -> Acceleration a = " + fa.getitem(0) + " " + fa.getitem(1) + " " + fa.getitem(2));
+                                    Console.WriteLine(" -> Angular velocity g = " + fg.getitem(0) + " " + fg.getitem(1) + " " + fg.getitem(2));
                                     Console.WriteLine(" -> Euler angles r = " + fr.getitem(0) + " " + +fr.getitem(1) + " " + fr.getitem(2));
                                     Console.WriteLine(" -> Quaternion w = " + fq.getitem(0) + " x " + +fq.getitem(1) + " y " + +fq.getitem(2) + " z " + + fq.getitem(3));
                                     break;
@@ -99,12 +102,21 @@ namespace OpenZenCSharpBindingTest
                 Thread.Sleep(1000);
             }
 
+            if (obj.mFoundSensors.Count == 0)
+            {
+                Console.WriteLine("No sensor found on the system");
+                OpenZen.ZenShutdown(zenHandle);
+                thr.Abort();
+                return;
+            }
+
             ZenSensorInitError sensorInitError = ZenSensorInitError.ZenSensorInitError_Max;
             // try three connection attempts
             for (int i = 0; i < 3; i++)
             {
                 ZenSensorHandle_t sensorHandle = new ZenSensorHandle_t();
                 sensorInitError = OpenZen.ZenObtainSensor(zenHandle, obj.mFoundSensors[0], sensorHandle);
+                Console.WriteLine("Connecting to sensor " + obj.mFoundSensors[0].identifier + " on IO interface " + obj.mFoundSensors[0].ioType);
                 if (sensorInitError == ZenSensorInitError.ZenSensorInitError_None)
                 {
                     Console.WriteLine("Succesfully connected to sensor");
